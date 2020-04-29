b556401936a738234e8e28e98dc2e564778fd013
Alexander Weaver <alex@AlexWMac.home>
2020-04-29 16:06:35 -0400

    added some gameboard logic

--- app/src/main/java/com/example/minesweeper/GameBoard.java
+++ app/src/main/java/com/example/minesweeper/GameBoard.java
@@ -1,17 +1,30 @@
 package com.example.minesweeper;
 
-public class GameBoard {
-    public Cell[][] grid;
-    int columns;
-    int rows;
+import java.util.Random;
 
+public class GameBoard {
     public GameBoard(){
-        columns = 7;
-        rows = 10;
-        grid = new Cell[columns][rows];
-        for (int i = 0; i < columns; i++){
-            for (int j = 0; j< rows; j++){
-                grid[i][j] = new Cell(i, j);
+        //make 1d array for 10 bombs
+        int[] buttons = new int[70];
+        Random r = new Random();
+        int bombs = 0;
+        while (bombs < 10) {
+            int loc = r.nextInt(70);
+            if (buttons[loc] == 1) {
+                continue;
+            } else {
+                buttons[loc] = 1;
+                bombs++;
+            }
+        }
+        //turn 1d array into 2d array
+        Cell[][] boardArray = new Cell[7][10];
+        int k = 0;
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 9; j++){
+                boardArray[i][j].setBomb(buttons[k]);
+                boardArray[i][j].setRow(i);
+                boardArray[i][j].setColumn(j);
             }
         }
     }
